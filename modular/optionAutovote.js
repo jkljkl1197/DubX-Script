a8b225ca2b085055e42a871b4b4c44fa936c5867
Brett <weisbrett@gmail.com>
2015-10-05 04:23:44 -0700

    Removed obfuscation

--- modular/optionAutovote.js
+++ modular/optionAutovote.js
@@ -1,1 +1,29 @@
-var _0x42ac=["\x6F\x6E","\x63\x6C\x69\x63\x6B","\x2E\x64\x75\x62\x75\x70","\x44\x4F\x4D\x53\x75\x62\x74\x72\x65\x65\x4D\x6F\x64\x69\x66\x69\x65\x64","\x62\x69\x6E\x64","\x2E\x63\x75\x72\x72\x65\x6E\x74\x53\x6F\x6E\x67","\x69\x73\x41\x75\x74\x6F\x76\x6F\x74\x65","\x74\x72\x75\x65","\x73\x65\x74\x49\x74\x65\x6D","\x3C\x69\x20\x63\x6C\x61\x73\x73\x3D\x22\x66\x69\x2D\x63\x68\x65\x63\x6B\x22\x3E\x3C\x2F\x69\x3E","\x72\x65\x70\x6C\x61\x63\x65\x57\x69\x74\x68","\x2E\x61\x59\x20\x2E\x69\x73\x4F\x6E\x4F\x66\x66\x20\x69","\x6F\x66\x66","\x66\x61\x6C\x73\x65","\x3C\x69\x20\x63\x6C\x61\x73\x73\x3D\x22\x66\x69\x2D\x78\x22\x3E\x3C\x2F\x69\x3E","\x67\x65\x74\x49\x74\x65\x6D","\x2E\x61\x59"];var isAutovote=false;function optionAutovote(){var _0x3e83x3;if(!isAutovote){isAutovote=true;_0x3e83x3=_0x42ac[0];$(_0x42ac[2])[_0x42ac[1]]();function _0x3e83x4(){$(_0x42ac[2])[_0x42ac[1]]()}$(_0x42ac[5])[_0x42ac[4]](_0x42ac[3],_0x3e83x4);localStorage[_0x42ac[8]](_0x42ac[6],_0x42ac[7]);$(_0x42ac[11])[_0x42ac[10]](_0x42ac[9]);}else {isAutovote=false;_0x3e83x3=_0x42ac[12];$(_0x42ac[5])[_0x42ac[12]]();localStorage[_0x42ac[8]](_0x42ac[6],_0x42ac[13]);$(_0x42ac[11])[_0x42ac[10]](_0x42ac[14]);};}if(localStorage[_0x42ac[15]](_0x42ac[6])===_0x42ac[7]){optionAutovote()};$(_0x42ac[16])[_0x42ac[1]](optionAutovote);
+//optionAutovote.js
+var isAutovote = false;
+
+function optionAutovote() {
+    var isOn;
+    if (!isAutovote) {
+        isAutovote = true;
+        isOn = "on";
+        $('.dubup').click();
+        function advanceVote() {
+            $('.dubup').click();
+        };
+        $('.currentSong').bind("DOMSubtreeModified", advanceVote);
+        localStorage.setItem('isAutovote','true');
+        $('.aY .isOnOff i').replaceWith('<i class="fi-check"></i>');
+    } else {
+        isAutovote = false;
+        isOn = "off";
+        $('.currentSong').off();
+        localStorage.setItem('isAutovote','false');
+        $('.aY .isOnOff i').replaceWith('<i class="fi-x"></i>');
+    }
+};
+
+if (localStorage.getItem('isAutovote') === 'true') {
+    optionAutovote();
+};
+
+$('.aY').click(optionAutovote);
