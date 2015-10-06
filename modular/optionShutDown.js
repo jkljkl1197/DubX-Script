3f4e11280c74cffc4347d189cc6e372160abf9d2
Brett <weisbrett@gmail.com>
2015-10-05 20:31:17 -0700

    Update optionShutDown.js

--- modular/optionShutDown.js
+++ modular/optionShutDown.js
@@ -1,22 +1,3 @@
 //optionShutDown.js
-var isShutDown = false;
-function optionShutDown() {
-	var isOn
-		if (!isShutDown) {
-			isShutDown = true
-			isOn = "on";
-			function chatLog(e){
-				var a=new Dubtrack.View.chatLoadingItem;a.$el.text(e).appendTo(Dubtrack.room.chat._messagesEl)
-			}
-			setTimeout(function(){chatLog('Shutting down Dub X..'); }, 1000);
-			setTimeout(function(){throw new Error('Shutting down..'); }, 3000);
-			};
-		} else {
-			//I did this for lolz
-			isShutDown = false
-			isOn = "off";
-			console.log("How did you get here?");
-		}
-};
 
-$('.pY').click(optionShutDown);
+//working on it
