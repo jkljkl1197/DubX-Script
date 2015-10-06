0fce135d50c1554576a63f4b246e3ea7bff9f59e
Brett <weisbrett@gmail.com>
2015-10-06 16:55:09 -0700

    Reverted back

--- modular/optionAutorespond.js
+++ modular/optionAutorespond.js
@@ -1,6 +1,5 @@
 //optionAutorespond.js
 var isAutorespond = false;
-var sendAutorespond = true;
 function realtimeChat(data) {
     var realtimeContent = data.message;
     var isUserAfk = Dubtrack.session.get('username');
@@ -11,22 +10,16 @@ function realtimeChat(data) {
 }
 function optionAutorespond() {
     var isOn
-    if (!isAutorespond) {
-        isAutorespond = true
-        isOn = "on";
-        if (sendAutorespond) {
+        if (!isAutorespond) {
+            isAutorespond = true
+            isOn = "on";
             Dubtrack.Events.bind('realtime:chat-message',realtimeChat);
             $('.nY .isOnOff i').replaceWith('<i class="fi-check"></i>');
+        } else {
+            isAutorespond = false
+            isOn = "off";
+            Dubtrack.Events.unbind('realtime:chat-message',realtimeChat);
+            $('.nY .isOnOff i').replaceWith('<i class="fi-x"></i>');
         }
-        sendAutorespond = false;
-        setTimeout (function(){
-            sendAutorespond = true;
-        }, 30 * 1000);
-    } else {
-        isAutorespond = false
-        isOn = "off";
-        Dubtrack.Events.unbind('realtime:chat-message',realtimeChat);
-        $('.nY .isOnOff i').replaceWith('<i class="fi-x"></i>');
-    }
 };
 $('.nY').click(optionAutorespond);
