99812ca997b09bbdcf2797ea05c688c14b1f767e
PossiblyKris <PossiblyKris@users.noreply.github.com>
2015-10-12 18:16:16 +0200

    Added cooldown to autoresponse

--- modular/optionAutorespond.js
+++ modular/optionAutorespond.js
@@ -1,24 +1,31 @@
 //optionAutorespond.js
 var isAutorespond = false;
+var sendAutorespond = true;
 function realtimeChat(data) {
     var realtimeContent = data.message;
     var isUserAfk = Dubtrack.session.get('username');
     if (realtimeContent.indexOf('@'+isUserAfk) >-1) {
-        $('#chat-txt-message').val('I am AFK at the moment.');
-        Dubtrack.room.chat.sendMessage();
+        if (sendAutorespond) {
+            $('#chat-txt-message').val('I am AFK at the moment.');
+            Dubtrack.room.chat.sendMessage();
+            sendAutorespond = false;
+            setTimeout (function() {
+                sendAutorespond = true;
+            }, 30 * 1000);
+        }
     }
 }
 function optionAutorespond() {
-    var isOn
+    var isOn;
         if (!isAutorespond) {
-            isAutorespond = true
+            isAutorespond = true;
             isOn = "on";
-            Dubtrack.Events.bind('realtime:chat-message',realtimeChat);
+            Dubtrack.Events.bind('realtime:chat-message', realtimeChat);
             $('.nY .isOnOff i').replaceWith('<i class="fi-check"></i>');
         } else {
-            isAutorespond = false
+            isAutorespond = false;
             isOn = "off";
-            Dubtrack.Events.unbind('realtime:chat-message',realtimeChat);
+            Dubtrack.Events.unbind('realtime:chat-message', realtimeChat);
             $('.nY .isOnOff i').replaceWith('<i class="fi-x"></i>');
         }
 };
