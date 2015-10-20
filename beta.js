5195f385e45e24b1921cf41f67a63d2e06376bc0
Brett <weisbrett@gmail.com>
2015-10-19 19:41:04 -0700

    Changed "Prevent Page" to "Warn On"

--- beta.js
+++ beta.js
@@ -106,7 +106,7 @@ if (!isOpen) {
                     '</li>',
                     '<li onclick="optionOnBeforeUnload();" class="optionClass onbeforeunload">',
                         '<p class="isOnOff"><i class="fi-x"></i></p>',
-                        '<p class="Optionling">Prevent Page Navigation</p>',
+                        '<p class="Optionling">Warn On Navigation</p>',
                     '</li>',
                     '<li onclick="communityCSS();" class="optionClass communitycss">',
                         '<p class="isOnOff"><i class="fi-x"></i></p>',
