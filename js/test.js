fb99df21357b52e672cd04d4561a5becbede6c28
franciscog <frankjg@gmail.com>
2015-10-26 22:26:38 -0400

    undoing to see if it was something I did

--- js/test.js
+++ js/test.js
@@ -424,10 +424,8 @@ if (!hello_run) {
                 }).done(function(e) {
                     var content = e.data.description;
                     var url = content.match(/(@dubx=)(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/);
-                    if (url && url[0]) {
-                        var append = url[0].split('@dubx=');
-                        $('head').append('<link class="css_world" href="'+append[1]+'" rel="stylesheet" type="text/css">');
-                    }
+                    var append = url[0].split('@dubx=');
+                    $('head').append('<link class="css_world" href="'+append[1]+'" rel="stylesheet" type="text/css">');
                 });
                 hello.option('css_world','true');
                 hello.on('.css');
@@ -471,73 +469,14 @@ if (!hello_run) {
                 var content = localStorage.getItem('medium');
                 $('body').append('<div class="medium" style="width: 100vw;height: 100vh;z-index: -999998;position: fixed; background: url('+content+');background-size: cover;top: 0;"></div>');
             }
-        },
-        emojiPreview: function(){
-            $('.pusher-chat-widget-input').prepend('<div id="emoji-preview" style="display: none; border: 1px solid #202020; position: absolute; bottom: 54px; background-color:#111;"></div>');
-            
-            function createImg(name) {
-                return [
-                    '<div style="display:inline-block">',
-                        '<img style="width: 1em; height: 1em; display:inline-block" src="'+emojify.defaultConfig.img_dir+'/'+encodeURI(name)+'.png" title=":'+name+':" alt=":'+name+':" align="absmiddle" />',
-                        '<span style="font-size: 0.8em; display:inline-block; padding-left: 3px;">:' + name + ':, </span>',
-                    '</div>'
-                ].join('');
-            }
-            function addToHelper(emojiArray) {
-                $('#emoji-preview').empty();
-                var text = "";
-                $.each(emojiArray, function(i,val){
-                    text += createImg(val);
-                });
-                $('#emoji-preview').append(text).show();
-            }
-            function filterEmoji(str){
-                return emojify.emojiNames.filter(function(val){
-                    var re = new RegExp('^' + str.replace(":","") ,"ig");
-                    return re.test(val);
-                });
-            }
-            function isGoodKey(x){
-                // 48 - 90  =  0-9 A-Z || 96 - 111 = numpad || 189 = dash
-                return (x >= 48 && x <= 90) || (x >= 96 && x <= 111) || x === 189;
-            }
-            var emojiColon = false;
-            var searchStr = "";
-            $("#chat-txt-message").keydown(function(e) {
-                if (!emojiColon && e.shiftKey && e.which === 186) {
-                    emojiColon = true;
-                } else if (emojiColon && e.shiftKey && e.which === 186) {
-                    emojiColon = false;
-                    searchStr = "";
-                    $('#emoji-preview').empty().hide();
-                }
-
-                if (e.which === 8 && searchStr.length > 0) {
-                    // backspace
-                    searchStr = searchStr.substring(0, searchStr.length - 1);
-                    addToHelper(filterEmoji(searchStr));
-                }
-
-                if (e.which === 8 && searchStr.length === 0) {
-                    // backspace has erased all characters so we reset
-                    emojiColon = false;
-                    $('#emoji-preview').empty().hide();
-                }
-                
-                if (emojiColon && isGoodKey(e.which)) {
-                    searchStr += String.fromCharCode(e.which);
-                    addToHelper(filterEmoji(searchStr));
-                }
-            });
         }
     };
     //Ref 3:
     hello.initialize();
     hello.personalize();
-    hello.emojiPreview();
-    // setInterval(function() {
-    //     hello.eta();
-    // }, 5000);
+    setInterval(function() {
+        hello.eta();
+    }, 5000);
     
     //Ref 4: 
     $('.user-info-button').click(hello.wide_video_disable);
