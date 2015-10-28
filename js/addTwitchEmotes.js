85395a8373d1857bb3c5579046e16a68e7ff7d6f
franciscog <frankjg@gmail.com>
2015-10-27 23:03:01 -0400

    back to js

--- js/addTwitchEmotes.js
+++ js/addTwitchEmotes.js
@@ -2,23 +2,22 @@
 // source:  https://twitchemotes.com/apidocs 
 var GitHubLocation = 'https://rawgit.com/FranciscoG/DubX-Script/dev/js/';
 
-$.getScript(GitHubLocation + 'twitchemotes.js')
-  .done(function(data){
-    console.log(data);
-    var twitchObject = JSON.parse(data);
-    var re = new RegExp(Object.keys(twitchObject.emotes).join("|"),"g"); 
+$.getScript(GitHubLocation + 'twitchemotes.js', function(data){
+  console.log(data);
+  
+  var re = new RegExp(Object.keys(twitchObject.emotes).join("|"),"g"); 
 
-    function makeImage(id){
-      return '<img class="emoji" src="//static-cdn.jtvnw.net/emoticons/v1/'+id+'/1.0" />';
-    }
-    function replaceTextWithEmote(){
-      var $last = $('.chat-main .text').last();
-      var emoted = $last.html().replace(re, function(matched){
-        return makeImage(twitchObject.emotes[matched].image_id);
-      });
-      $last.html(emoted);
-    }
-    Dubtrack.Events.bind("realtime:chat-message", replaceTextWithEmote);
-  });
+  function makeImage(id){
+    return '<img class="emoji" src="//static-cdn.jtvnw.net/emoticons/v1/'+id+'/1.0" />';
+  }
+  function replaceTextWithEmote(){
+    var $last = $('.chat-main .text').last();
+    var emoted = $last.html().replace(re, function(matched){
+      return makeImage(twitchObject.emotes[matched].image_id);
+    });
+    $last.html(emoted);
+  }
+  Dubtrack.Events.bind("realtime:chat-message", replaceTextWithEmote);
+});
 
 
