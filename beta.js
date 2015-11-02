eabdb651e7085bb8f90a33191a696400f0b201d5
Brett <weisbrett@gmail.com>
2015-11-02 08:45:55 -0800

    Update user count

--- beta.js
+++ beta.js
@@ -170,7 +170,7 @@ if (!hello_run) {
                             '</a>',
                         '</li>',
                         '<li class="optionClass">',
-                        	'<p class="Optionful" style="margin: 0;">Dub X currently has 990 users!</p>',
+                        	'<p class="Optionful" style="margin: 0;">Dub X currently has 977 users!</p>',
                         '</li>',
                     '</ul>',
                 '</div>'
