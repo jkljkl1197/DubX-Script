af0a3bd6176e3f007bf33083e6fd6853c6b6f568
Swordling <l1998edwards@gmail.com>
2015-11-03 20:08:24 +1000

    Fixed a mistake.

--- beta.js
+++ beta.js
@@ -465,7 +465,7 @@ if (!hello_run) {
         medium_import: function() {
             var content = $('.input').val();
             localStorage.setItem('medium',content);
-            $('.custom_wallpaper').remove();
+            $('.medium').remove();
             $('body').append('<div class="medium" style="width: 100vw;height: 100vh;z-index: -999998;position: fixed; background: url('+content+');background-size: cover;top: 0;"></div>');
             $('.onErr').remove();
         },
