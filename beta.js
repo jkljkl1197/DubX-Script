878eda51b8889989fc29b7cf46c02d91cf598e55
Swordling <l1998edwards@gmail.com>
2015-11-08 14:34:19 +1000

    Update again

--- beta.js
+++ beta.js
@@ -29,7 +29,7 @@
 var hello_run;
 if (!hello_run) {
     hello_run = true;
-    var our_version = '03.SAFE.00 - ROLLBACK... SORRY!';
+    var our_version = '03.01.03 - VIDEO ONLY';
 
     //Ref 1: Variables
     var options = {
@@ -136,6 +136,18 @@ if (!hello_run) {
                             '<p class="for_content_c">Settings</p>',
                         '</li>',
                         '<ul class="draw_settings">',
+                            '<li onclick="hello.optionTwitchEmotes();" class="for_content_li for_content_feature twitch_emotes">',
+                                '<p class="for_content_off"><i class="fi-x"></i></p>',
+                                '<p class="for_content_p">Twitch Emotes</p>',
+                            '</li>',
+                            '<li onclick="hello.optionEmojiPreview();" class="for_content_li for_content_feature emoji_preview">',
+                                '<p class="for_content_off"><i class="fi-x"></i></p>',
+                                '<p class="for_content_p">Emoji Preview</p>',
+                            '</li>',
+                            '<li onclick="hello.spacebar_mute();" class="for_content_li for_content_feature spacebar_mute">',
+                                '<p class="for_content_off"><i class="fi-x"></i></p>',
+                                '<p class="for_content_p">Spacebar Mute</p>',
+                            '</li>',
                             '<li onclick="hello.warn_redirect();" class="for_content_li for_content_feature warn_redirect">',
                                 '<p class="for_content_off"><i class="fi-x"></i></p>',
                                 '<p class="for_content_p">Warn On Navigation</p>',
@@ -506,6 +518,313 @@ if (!hello_run) {
                 hello.off('.video_window');
             }
         },
+        // jQuery's getJSON kept returning errors so making my own with promise-like
+        // structure and added optional Event to fire when done so can hook in elsewhere
+        getJSON : (function (url, optionalEvent) {
+            var doneEvent;
+            function GetJ(_url, _cb){
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', _url);
+                xhr.send();
+                xhr.onload = function() {
+                    var resp = xhr.responseText;
+                    if (typeof _cb === 'function') { _cb(resp); }
+                    if (optionalEvent) { document.dispatchEvent(doneEvent); }
+                };
+            }
+            if (optionalEvent){ doneEvent = new CustomEvent(optionalEvent); }
+            var done = function(cb){
+                new GetJ(url, cb);
+            };
+            return { done: done };
+        }),
+
+        twitch : { 
+            template: "//static-cdn.jtvnw.net/emoticons/v1/{image_id}/3.0",
+            specialEmotes: [],
+            emotes: {},
+            chatRegex : new RegExp(":([-_a-z0-9]+):", "ig")
+        },
+        /**************************************************************************
+         * Loads the twitch emotes from the api.  
+         * http://api.twitch.tv/kraken/chat/emoticon_images
+         */
+        loadTwitchEmotes: function(){
+            var self = this;
+            var day = 86400000; // milliseconds
+            var savedData;
+
+            var today = Date.now();
+            var lastSaved = parseInt(localStorage.getItem('twitch_api_timestamp'));
+            
+            // if it doesn't exist in localStorage or it's older than 5 days
+            // grab it from the twitch API
+            if (isNaN(lastSaved) || today - lastSaved > day * 5 || !localStorage.twitch_api) {
+                console.log('Dubx','twitch','loading from api');
+                this.getJSON('//api.twitch.tv/kraken/chat/emoticon_images', 'emotes:loaded')
+                    .done(function(data){
+                        localStorage.setItem('twitch_api_timestamp', Date.now().toString());
+                        localStorage.setItem('twitch_api', data);
+                        self.processEmotes(JSON.parse(data));
+                    });
+            } else {
+                console.log('Dubx','twitch','loading from localstorage');
+                savedData = JSON.parse(localStorage.getItem('twitch_api'));
+                self.processEmotes(savedData);
+            }
+            
+        },
+        processEmotes: function(data) {
+            var self = hello;
+            data.emoticons.forEach(function(el,i,arr){
+                var _key = el.code.toLowerCase();
+                
+                // move twitch non-named emojis to their own array
+                if (el.code.indexOf('\\') >= 0) {
+                    self.twitch.specialEmotes.push([el.code, el.id]);
+                    return;
+                }
+
+                if (emojify.emojiNames.indexOf(_key) >= 0) {
+                    return; // do nothing so we don't override emoji
+                }
+                
+                if (!self.twitch.emotes[_key]){
+                    // if emote doesn't exist, add it
+                    self.twitch.emotes[_key] = el.id;
+                } else if (el.emoticon_set === null) {
+                    // override if it's a global emote (null set = global emote)
+                    self.twitch.emotes[_key] = el.id;
+                }
+                
+            });
+            self.twitchJSONSLoaded = true;
+            self.emojiTwitch = emojify.emojiNames.concat(Object.keys(self.twitch.emotes));
+        },
+        /**************************************************************************
+         * handles replacing twitch emotes in the chat box with the images
+         */
+        
+        replaceTextWithEmote: function(){
+            var self = hello;
+
+            if (!self.twitchJSONSLoaded) { return; } // can't do anything until jsons are loaded
+            function makeImage(src, name){
+                return '<img class="emoji twitch-emoji" title="'+name+'" alt="'+name+'" src="'+src+'" />';
+            }
+            
+            var $last = $('.chat-main .text').last();
+            if (!$last.html()) { return; } // nothing to do
+            
+            var emoted = $last.html().replace(self.twitch.chatRegex, function(matched, p1){
+                var _id, _src, _desc, key = p1.toLowerCase();
+                if (typeof self.twitch.emotes[key] !== 'undefined'){
+                    _id = self.twitch.emotes[key];
+                    _src = self.twitch.template.replace("{image_id}", _id);
+                    return makeImage(_src, key);
+                } else {
+                    return matched;
+                }
+            });
+            $last.html(emoted);
+        },
+        /**************************************************************************
+         * Turn on/off the twitch emoji in chat
+         */
+        optionTwitchEmotes: function(){
+            if (!options.let_twitch_emotes) {
+
+                if (!hello.twitchJSONSLoaded) {
+                    hello.loadTwitchEmotes();
+                    document.addEventListener('emotes:loaded', this.replaceTextWithEmote);
+                } else {
+                    this.replaceTextWithEmote();
+                }
+                
+                Dubtrack.Events.bind("realtime:chat-message", this.replaceTextWithEmote);
+                options.let_twitch_emotes = true;
+                hello.option('twitch_emotes', 'true');
+                hello.on('.twitch_emotes');
+            } else {
+                Dubtrack.Events.unbind("realtime:chat-message", this.replaceTextWithEmote);
+                options.let_twitch_emotes = false;
+                hello.option('twitch_emotes', 'false');
+                hello.off('.twitch_emotes');
+            }
+        },
+        /**************************************************************************
+         * A bunch of utility helpers for the emoji preview
+         */
+        emojiUtils : {
+            makePreviewContainer : function(cn){
+                var d = document.createElement('li');
+                d.className = cn;
+                return d; 
+            },
+            makeEmoImage : function(src) {
+                var i = document.createElement('img');
+                i.src = src;
+                return i;
+            },
+            makeNameSpan : function(name){
+                var s = document.createElement('span');
+                s.textContent = ":" + name + ":";
+                return s;
+            },
+            makeLi: function(type, name, img){
+                var self = hello.emojiUtils;
+                var container = self.makePreviewContainer("preview-container "+type+"-previews");
+                var span = self.makeNameSpan(name);
+                container.appendChild(img);
+                container.appendChild(span);
+                container.tabIndex = -1;
+                return container;
+            },
+            createTwitchImg : function(id, name) {
+                var self = hello.emojiUtils;
+                var _src = hello.twitch.template.replace("{image_id}", id);
+                var img = self.makeEmoImage(_src);
+                img.title = name; img.alt = name;
+                return self.makeLi('twitch', name, img);
+            },
+            createImg : function(name) {
+                var self = hello.emojiUtils;
+                var img = self.makeEmoImage(emojify.defaultConfig.img_dir+'/'+encodeURI(name)+'.png');
+                img.title = ':'+name+':'; 
+                return self.makeLi('emoji', name, img);
+            },
+            addToHelper : function(emojiArray) {
+                var self = hello.emojiUtils;
+                $('#emoji-preview').empty();
+                var frag = document.createDocumentFragment();
+                var _key;
+
+                emojiArray.forEach(function(val,i,arr){
+                    _key = val.toLowerCase();
+                    if (typeof hello.twitch.emotes[_key] !== 'undefined') {
+                        frag.appendChild(self.createTwitchImg(hello.twitch.emotes[_key], val));
+                    } else if (emojify.emojiNames.indexOf(_key) >= 0) {
+                        frag.appendChild(self.createImg(val));
+                    }
+                });
+
+                document.getElementById('emoji-preview').appendChild(frag);
+                $('#emoji-preview').addClass('emoji-grow');
+            },
+            filterEmoji : function(str){
+                var finalStr = str.replace("+","\\+");
+                var re = new RegExp('^' + finalStr, "i");
+                var arrayToUse = emojify.emojiNames;
+                if (options.let_twitch_emotes) {
+                    arrayToUse = hello.emojiTwitch; // merged array
+                }
+                return arrayToUse.filter(function(val){
+                    return re.test(val);
+                });
+            },
+            emojiSearchStr : ""
+        },
+        /**************************************************************************
+         * This handles the emoji preview in the chat input as you type
+         */
+        emojiKeyUpFunction: function(e){
+            var self = hello.emojiUtils;
+            var currentText = this.value;
+            var filteredEmoji = currentText.replace(/:([+\\-_a-z0-9]+)$/i, function(matched, p1){
+                self.emojiSearchStr = p1;
+                if (self.emojiSearchStr.length >= 3) { // change to set character limit
+                    self.addToHelper(self.filterEmoji(p1));
+                }
+            });
+            
+            var lastChar = currentText.charAt(currentText.length - 1);
+            if (self.emojiSearchStr.length <= 2 || // change to set character limit
+                lastChar === ":" ||
+                lastChar === " " ||
+                currentText === "")
+            {
+                self.emojiSearchStr = "";
+                $('#emoji-preview').empty().removeClass('emoji-grow');
+            }
+
+            if ($('.emoji-grow li').length === 1) {
+                $('.emoji-grow li').append('<span>press &darr; to select</span>').addClass('selected');
+            }
+
+            if ($('.emoji-grow li').length === 1 && e.keyCode === 40) {
+                $('#emoji-preview li.selected').trigger('click');
+                return;
+            }
+
+            if (e.keyCode === 38 || e.keyCode === 40) {
+                hello.doNavigate(-1);
+            }
+        },
+        displayBoxIndex : -1,
+        doNavigate : function(diff) {
+            hello.displayBoxIndex += diff;
+            var oBoxCollection = $(".emoji-grow li");
+            if (hello.displayBoxIndex >= oBoxCollection.length){
+                hello.displayBoxIndex = 0;
+            }
+            if (hello.displayBoxIndex < 0){
+                 hello.displayBoxIndex = oBoxCollection.length - 1;
+             }
+            var cssClass = "selected";
+            oBoxCollection.removeClass(cssClass).eq(hello.displayBoxIndex).addClass(cssClass).focus();
+        },
+        emojiKeyNavFunction: function(e){
+            if ( $('#emoji-preview').hasClass('emoji-grow')) {
+               e.preventDefault();
+               if (e.keyCode === 38) {
+                   hello.doNavigate(-1);
+               }
+               else if (e.keyCode === 40) {
+                   hello.doNavigate(1);
+               }
+               else if (e.keyCode === 13) {
+                   $('#emoji-preview li.selected').trigger('click');
+                   return false;
+               }
+            } 
+        },
+        updateChatInput: function(str){
+            var _re = new RegExp(":"+hello.emojiUtils.emojiSearchStr + "$");
+            var fixed_text = $("#chat-txt-message").val().replace(_re, str) + " ";
+            $('#emoji-preview').empty().removeClass('emoji-grow');
+            $("#chat-txt-message").val(fixed_text).focus();
+        },
+        emojiTwitchInit: function(){
+            // this will only be run once
+            $('head').prepend('<link rel="stylesheet" type="text/css" href="'+hello.gitRoot+'/css/options/emoji.css">');
+            var emojiPreview = document.createElement('ul');
+            emojiPreview.id = "emoji-preview";
+            $('.pusher-chat-widget-input').prepend(emojiPreview);
+
+            $(document.body).on('click', '.preview-container', function(e){
+                var new_text = $(this).find('span')[0].textContent;
+                hello.updateChatInput(new_text);
+            });
+        },
+        optionEmojiPreview: function(){
+            if (!$('#emoji-preview').length) {
+                hello.emojiTwitchInit();
+            }
+
+            if (!options.let_emoji_preview) {
+                $(document.body).on('keyup', "#chat-txt-message", this.emojiKeyUpFunction);
+                $(document.body).on('keyup', '.emoji-grow', hello.emojiKeyNavFunction);
+                options.let_emoji_preview = true;
+                hello.option('emoji_preview', 'true');
+                hello.on('.emoji_preview');
+            } else {
+                $(document.body).off('keyup', "#chat-txt-message", this.emojiKeyUpFunction);
+                $(document.body).off('keyup', '.emoji-grow', hello.emojiKeyNavFunction);
+                options.let_emoji_preview = false;
+                hello.option('emoji_preview', 'false');
+                hello.off('.emoji_preview');
+            }
+        },
         spacebar_mute: function() {
             if (!options.let_spacebar_mute) {
                 options.let_spacebar_mute = true;
@@ -557,6 +876,12 @@ if (!hello_run) {
     if (localStorage.getItem('nicole') === 'true') {
         hello.nicole();
     }
+    if (localStorage.getItem('twitch_emotes') === 'true') {
+        hello.optionTwitchEmotes();
+    }
+    if (localStorage.getItem('emoji_preview') === 'true') {
+        hello.optionEmojiPreview();
+    }
     if (localStorage.getItem('spacebar_mute') === 'true') {
         hello.spacebar_mute();
     }
@@ -568,6 +893,13 @@ if (!hello_run) {
     });
     
     // Ref 5:
+    $('.chat-main').on('DOMNodeInserted', function(e) {
+        var itemEl = $(e.target);
+        if(itemEl.prop('tagName').toLowerCase() !== 'li' || itemEl.attr('class').substring(0, 'user-'.length) !== 'user-') return;
+        var user = Dubtrack.room.users.collection.findWhere({userid: itemEl.attr('class').split(/-| /)[1]});
+        var role = !user.get('roleid') ? 'default' : Dubtrack.helpers.isDubtrackAdmin(user.get('userid')) ? 'admin' : user.get('roleid').type;
+        itemEl.addClass('is' + (role.charAt(0).toUpperCase() + role.slice(1)));
+    });
 
 } else {
     function onErr(error) {
