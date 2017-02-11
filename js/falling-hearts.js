522892d7b79e989544b612d42f179fc745852c89
jkljkl1197 <jordan1197@hotmail.ca>
2017-02-10 21:23:47 -0500

    Update falling-hearts.js

--- js/falling-hearts.js
+++ js/falling-hearts.js
@@ -1,66 +1,20 @@
-// From https://codepen.io/bferioli/pen/qEGaPp
-
-var HeartsBackground = {
-  heartHeight: 60,
-  heartWidth: 64,
-  hearts: [],
-  heartImage: 'https://puu.sh/tXG5z/0bbfe82e5e.png',
-  maxHearts: 8,
-  minScale: 0.4,
-  draw: function() {
-    this.setCanvasSize();
-    this.ctx.clearRect(0, 0, this.w, this.h);
-    for (var i = 0; i < this.hearts.length; i++) {
-      var heart = this.hearts[i];
-      heart.image = new Image();
-      heart.image.style.height = heart.height;
-      heart.image.src = this.heartImage;
-      this.ctx.globalAlpha = heart.opacity;
-      this.ctx.drawImage (heart.image, heart.x, heart.y, heart.width, heart.height);
-    }
-    this.move();
-  },
-  move: function() {
-    for(var b = 0; b < this.hearts.length; b++) {
-      var heart = this.hearts[b];
-      heart.y += heart.ys;
-      if(heart.y > this.h) {
-        heart.x = Math.random() * this.w;
-        heart.y = -1 * this.heartHeight;
-      }
-    }
-  },
-  setCanvasSize: function() {
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-    this.w = this.canvas.width;
-    this.h = this.canvas.height;
-  },
-  initialize: function() {
-    this.canvas = $('#canvas')[0];
-
-    if(!this.canvas.getContext)
-      return;
-
-    this.setCanvasSize();
-    this.ctx = this.canvas.getContext('2d');
-
-    for(var a = 0; a < this.maxHearts; a++) {
-      var scale = (Math.random() * (1 - this.minScale)) + this.minScale;
-      this.hearts.push({
-        x: Math.random() * this.w,
-        y: Math.random() * this.h,
-        ys: Math.random() + 1,
-        height: scale * this.heartHeight,
-        width: scale * this.heartWidth,
-        opacity: scale
-      });
-    }
-
-    setInterval($.proxy(this.draw, this), 30);
-  }
-};
-
-$(document).ready(function(){
-  HeartsBackground.initialize();
-});
+<marquee behavior=scroll direction=down scrollamount=3 scrolldelay=26 height=653 style='position:absolute; left:17%; top:103px; width:17; height:653px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=4 scrolldelay=41 height=668 style='position:absolute; left:61%; top:27px; width:17; height:668px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=5 scrolldelay=44 height=522 style='position:absolute; left:93%; top:23px; width:17; height:522px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=3 scrolldelay=14 height=576 style='position:absolute; left:69%; top:154px; width:17; height:576px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=6 scrolldelay=32 height=520 style='position:absolute; left:59%; top:140px; width:17; height:520px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=3 scrolldelay=21 height=528 style='position:absolute; left:87%; top:125px; width:17; height:528px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=2 scrolldelay=27 height=574 style='position:absolute; left:89%; top:187px; width:17; height:574px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=1 scrolldelay=24 height=593 style='position:absolute; left:91%; top:21px; width:17; height:593px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=1 scrolldelay=48 height=558 style='position:absolute; left:9%; top:166px; width:17; height:558px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=4 scrolldelay=34 height=632 style='position:absolute; left:29%; top:48px; width:17; height:632px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=5 scrolldelay=43 height=610 style='position:absolute; left:53%; top:118px; width:17; height:610px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=5 scrolldelay=40 height=562 style='position:absolute; left:86%; top:133px; width:17; height:562px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=5 scrolldelay=43 height=514 style='position:absolute; left:91%; top:142px; width:17; height:514px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=4 scrolldelay=7 height=525 style='position:absolute; left:44%; top:120px; width:17; height:525px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=2 scrolldelay=29 height=548 style='position:absolute; left:77%; top:108px; width:17; height:548px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=5 scrolldelay=25 height=692 style='position:absolute; left:93%; top:50px; width:17; height:692px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=4 scrolldelay=40 height=613 style='position:absolute; left:22%; top:110px; width:17; height:613px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=1 scrolldelay=14 height=519 style='position:absolute; left:70%; top:7px; width:17; height:519px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=4 scrolldelay=26 height=687 style='position:absolute; left:69%; top:193px; width:17; height:687px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
+<marquee behavior=scroll direction=down scrollamount=1 scrolldelay=20 height=526 style='position:absolute; left:15%; top:183px; width:17; height:526px;'><img src=http://dl3.glitter-graphics.net/pub/12/12603tbcu2fkuwr.gif border=0></marquee>
