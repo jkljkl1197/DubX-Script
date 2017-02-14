508e1c07635167e6507e38b736bc58b230558d30
jkljkl1197 <jordan1197@hotmail.ca>
2017-02-13 20:11:04 -0500

    Update jquery-sakura.js

--- js/jquery-sakura.js
+++ js/jquery-sakura.js
@@ -81,10 +81,10 @@
         var defaults = {
             blowAnimations: ['blow-soft-left', 'blow-medium-left', 'blow-soft-right', 'blow-medium-right'],
             className: 'sakura',
-            fallSpeed: 1,
+            fallSpeed: 100,
             maxSize: 14,
             minSize: 10,
-            newOn: 300,
+            newOn: 1000,
             swayAnimations: ['sway-0', 'sway-1', 'sway-2', 'sway-3', 'sway-4', 'sway-5', 'sway-6', 'sway-7', 'sway-8']
         };
 
