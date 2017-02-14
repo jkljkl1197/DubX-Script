a6cafa5e848d17fcbf8306f969a624b5029dd0f1
jkljkl1197 <jordan1197@hotmail.ca>
2017-02-13 19:26:41 -0500

    Update beta.js

--- beta.js
+++ beta.js
@@ -495,14 +495,14 @@ if (!hello_run && Dubtrack.session.id) {
                 hello.option('sakura','true');
                 hello.on('.sakura');
                 $(function() {
-        	$('backstretch').sakura();
+        	$('body').sakura();
     		});
             } else {
                 options.let_sakura = false;
                 hello.option('sakura','false');
                 hello.off('.sakura');
                 $(function() {
-        	$('backstretch').sakura('stop');
+        	$('body').sakura('stop');
     		});
             }
         },
