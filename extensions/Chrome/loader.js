61b0a84d1acfea5d0f75dc81140b2d1a38e0b0a3
jkljkl1197 <jordan1197@hotmail.ca>
2017-02-19 14:43:40 -0500

    Update loader.js

--- extensions/Chrome/loader.js
+++ extensions/Chrome/loader.js
@@ -3,3 +3,16 @@ setTimeout(function(){
     $.getScript('https://rawgit.com/jkljkl1197/DubX-Script/MyVersion/beta.js');
     $('.cake').remove();
 }, 5000);
+
+function getword(info,tab) {
+  console.log("Word " + info.selectionText + " was clicked.");
+  chrome.tabs.create({  
+    url: "http://www.google.com/search?q=" + info.selectionText,
+  });           
+}
+
+chrome.contextMenus.create({
+  title: "Search: %s", 
+  contexts:["selection"], 
+  onclick: getword,
+});
