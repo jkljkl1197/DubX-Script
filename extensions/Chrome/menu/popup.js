aae0ca58738e8e3a574a19eba1611bc25e7b6378
jkljkl1197 <jordan1197@hotmail.ca>
2017-02-19 17:03:01 -0500

    Update popup.js

--- extensions/Chrome/menu/popup.js
+++ extensions/Chrome/menu/popup.js
@@ -1,7 +1,22 @@
 // var app = chrome.runtime.getBackgroundPage();
 
-function hello() {
-  
+function reload() {
+	chrome.runtime.reload();
 }
 
-document.getElementById('clickme').addEventListener('click', hello);
+function website() {
+  openInNewTab('https://dubx.net/');
+}
+
+function donate() {
+  openInNewTab('https://dubx.net/donate.html');
+}
+
+function openInNewTab(url) {
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+
+document.getElementById('reload').addEventListener('click', reload);
+document.getElementById('website').addEventListener('click', website);
+document.getElementById('donate').addEventListener('click', donate);
